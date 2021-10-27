082dd71cadecd2def0d90f866329e96a4d306184
Angellus Paixão <angellus.paixao@oi.net.br>
2021-10-27 13:34:44 -0300

    Mudança na estrutura de pasta do serviço

--- front/src/components/ToDos/index.jsx
+++ front/src/components/ToDos/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { api } from "../services/api"
+import { api } from "../../services/api"
 import styles from './styles.module.scss'
 import ToDoItem from './item'
 
